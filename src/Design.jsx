4a88d7f5df5afaedc3bfbe36e903d28a2b959df9
DePrincio <deprincio@DePrincio.local>
2020-12-12 13:44:18 -0500

    first commit

---
+++ src/Design.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import "./App.css"
+
+export default function Design(){
+    <div className="container">
+        <div className="app-container">
+            <div className="head-container">
+                <h1>Todo App</h1>
+                <div className="input-wrapper">
+                    <input type="text"/>
+                    <button>add</button>
+                </div>
+            </div>
+            <div className="body-container">
+                <div className="todo-container">
+                    <p>Shopping</p>
+                    <button>add</button>
+                    <button>edit</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+}
